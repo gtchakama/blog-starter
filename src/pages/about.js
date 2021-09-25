41a764d6ad74006a67fc147bd18a42b3af32c016
George_Chakama <gtchakama@gmail.com>
2021-09-25 15:51:01 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>
-        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      I'm quietly confident, naturally curious, and perpetually working on improving my chops solving one problem at a time.
       </p>
     </Layout>
   );
